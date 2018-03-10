8f0435eb90a60648168f2f99b288d9311402eb26
DrWrzipx <matic.drobez@gmail.com>
2018-03-10 13:15:07 +0100

    First node-chain in C#

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First node in chain
+            Node first = new Node { Value = 3 };
+         
+            // Second node in chain
+            Node second = new Node { Value = 5 };
+
+            // Third node in chain
+            Node third = new Node { Value = 7 };
+
+            first.Next = second;
+            second.Next = third;
+
+            PrintList(first);
+        }
+
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
